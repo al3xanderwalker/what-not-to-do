33d19ee9b509ae40dc742cc51e2d218487ae8cbe
Alex Walker <48996881+al3xanderwalker@users.noreply.github.com>
2024-05-07 17:20:51 +0100

    Create a simple backend that implements CRUD for posts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Post as PostData } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPosts(): Promise<PostData[]> {
+    return this.appService.getPosts();
+  }
+
+  @Post()
+  createPost(data: { title: string; content: string }): Promise<PostData> {
+    return this.appService.createPost(data);
+  }
+
+  @Patch()
+  updatePost(data: {
+    id: number;
+    title: string;
+    content: string;
+  }): Promise<PostData> {
+    return this.appService.updatePost(data);
+  }
+
+  @Delete()
+  deletePost(data: { id: number }): Promise<PostData> {
+    return this.appService.deletePost(data);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Post } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  getPosts(): Promise<Post[]> {
+    return this.prisma.post.findMany();
+  }
+
+  createPost(data: { title: string; content: string }): Promise<Post> {
+    return this.prisma.post.create({ data });
+  }
+
+  updatePost(data: {
+    id: number;
+    title: string;
+    content: string;
+  }): Promise<Post> {
+    return this.prisma.post.update({
+      where: { id: data.id },
+      data: { title: data.title, content: data.content },
+    });
+  }
+
+  deletePost(data: { id: number }): Promise<Post> {
+    return this.prisma.post.delete({ where: { id: data.id } });
   }
 }
